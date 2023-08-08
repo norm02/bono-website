672b0571503858df3ec7c410b1280529320b5569
norm02 <richa0920@gmail.com>
2023-08-08 15:34:35 +0900

    Blogとenvを追加（未完成）

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,14 +2,9 @@ import { NextPage } from "next";
 
 const Footer: NextPage = () => {
   return (
-    <div id="Footer" className="bg-blue-300 py-8 px-6 flex justify-center">
+    <div id="Footer" className="py-8 px-6 flex justify-center">
       <div className="font-raleway text-sm">
-        <a
-          className="link text-white"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="link" href="" target="_blank" rel="noopener noreferrer">
           ©bono
         </a>
       </div>
