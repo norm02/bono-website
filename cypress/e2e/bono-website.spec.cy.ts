a36965e97f5227816440dc4bfb71876e5839ce85
norm02 <richa0920@gmail.com>
2023-08-14 00:54:12 +0900

    cypress更新

--- cypress/e2e/bono-website.spec.cy.ts
+++ cypress/e2e/bono-website.spec.cy.ts
@@ -1,9 +1,18 @@
-describe("bono-websiteのE2Eテスト", () => {
-  it("Top画面のテキストが想定通りか確認する", () => {
+describe("TopからFooterまでの一通りの動きをテストする", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+  it("Topのテキストやスクロールをテストする", () => {
     cy.get("#top").contains("bono-website");
     cy.get("#top").contains("ABOUT");
+    cy.get(".bg-orange-300").click();
+    cy.contains("ABOUT ME").should("be.visible");
+    cy.get("#about").should("be.visible");
     cy.get("#top").contains("SKILLS");
+    cy.get(".bg-green-300").click();
+    cy.contains("SKILLS").should("be.visible");
     cy.get("#top").contains("ACHIEVEMENT");
+    cy.get(".bg-indigo-300").click();
+    cy.contains("ACHIEVEMENT").should("be.visible");
   });
 });
