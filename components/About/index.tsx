461aaf91554fdc7bba667ae19567b412e65306c0
norm02 <richa0920@gmail.com>
2023-08-20 02:24:54 +0900

    背景色とテキスト色のコントラスト、テキストの大きさ、mouseoverの挙動の見直し

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next";
 
 const About: NextPage = () => {
   return (
-    <div id="about" className="bg-blue-300 px-6 min-h-screen">
+    <div id="about" className="bg-blue-100 px-6 min-h-screen">
       <div className="flex flex-col items-center justify-center w-11/12 max-w-2xl m-auto">
-        <div className="text-white text-4xl pt-40">ABOUT ME</div>
+        <div className="text-blue-600 text-4xl pt-40">ABOUT ME</div>
         <div className="flex items-center justify-center mt-8">
           <div className="flex flex-col items-center justify-center w-1/3">
             <img
@@ -12,7 +12,7 @@ const About: NextPage = () => {
               className="w-1/2 transform transition duration-300 hover:-translate-y-2"
               src="/images/miso-icon.png"
             />
-            <div className="text-white p-2 text-center w-44 rounded-md text-2xl mb-4">
+            <div className="text-blue-600 p-2 text-center w-44 rounded-md text-2xl mb-4">
               bono
             </div>
             <div className="flex items-center justify-center mt-4">
@@ -52,17 +52,17 @@ const About: NextPage = () => {
             </div>
           </div>
           <div className="flex items-center justify-center w-2/3">
-            <div className="bg-gray-500 p-4 rounded-md shadow-md overflow-y-auto">
-              <p className="text-white text-2xl">
+            <div className="bg-gray-100 p-4 rounded-md shadow-md overflow-y-auto">
+              <p className="text-gray-600 text-2xl">
                 Web開発の、ジュニアエンジニアを目指しています。
               </p>
-              <p className="text-white text-2xl">
+              <p className="text-gray-600 text-2xl">
                 よしなに動く、ツールや言語が好きです。
               </p>
-              <p className="text-white text-2xl">
+              <p className="text-gray-600 text-2xl">
                 地道にぼちぼち進めることが、モットーです。
               </p>
-              <p className="text-white text-2xl">
+              <p className="text-gray-600 text-2xl">
                 アルバイトでテスターを経験し、テスト自動化に取り組んでいます。
               </p>
             </div>
