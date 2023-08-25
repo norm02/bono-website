2ca8ecbf464bbddde6f94b3f9965a0cb805a30cc
norm02 <richa0920@gmail.com>
2023-08-25 20:18:29 +0900

    Cypressテスト内容見直し

--- cypress/e2e/bono-website.spec.cy.ts
+++ cypress/e2e/bono-website.spec.cy.ts
@@ -5,15 +5,12 @@ describe("TopからFooterまでの一通りの動きをテストする", () => {
   });
   it("Topのテキストやスクロールをテストする", () => {
     cy.get("#top").contains("bono-website");
-    cy.get("#top").contains("ABOUT");
-    cy.get(".bg-orange-300").click();
+    cy.get("#top").contains("ABOUT").click();
     cy.contains("ABOUT ME").should("be.visible");
     cy.get("#about").should("be.visible");
-    cy.get("#top").contains("SKILLS");
-    cy.get(".bg-green-300").click();
+    cy.get("#top").contains("SKILLS").click();
     cy.contains("SKILLS").should("be.visible");
-    cy.get("#top").contains("ACHIEVEMENT");
-    cy.get(".bg-indigo-300").click();
+    cy.get("#top").contains("ACHIEVEMENT").click();
     cy.contains("ACHIEVEMENT").should("be.visible");
   });
   it("cy-axeでアクセシビリティをテストする", () => {
